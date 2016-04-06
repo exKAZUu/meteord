f337c40cd63894aa56778d53071059a2f5ce1de8
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-04-06 20:21:09 +0900

    Update init.sh

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -1,7 +1,10 @@
 set -e
 
+echo "apt-get install --reinstall tzdata"
 apt-get install --reinstall tzdata
+echo "ls -al /usr/share/zoneinfo"
 ls -al /usr/share/zoneinfo
+echo "bash $METEORD_DIR/lib/install_base.sh"
 bash $METEORD_DIR/lib/install_base.sh
 bash $METEORD_DIR/lib/install_node.sh
 bash $METEORD_DIR/lib/install_phantomjs.sh
