c1bec5d23688bbeb222ee6fa6ed80b6fb258c701
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-04-06 20:28:23 +0900

    Update init.sh

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -2,8 +2,8 @@ set -e
 
 echo "apt-get install --reinstall tzdata"
 apt-get install --reinstall tzdata
-echo "ls -al /usr/share/zoneinfo"
-ls -al /usr/share/zoneinfo
+echo "ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime"
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 echo "bash $METEORD_DIR/lib/install_base.sh"
 bash $METEORD_DIR/lib/install_base.sh
 bash $METEORD_DIR/lib/install_node.sh
