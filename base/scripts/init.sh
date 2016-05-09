3e669f392078c370a550285abb7c16787266a925
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-05-09 21:33:27 +0900

    Update init.sh

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -1,6 +1,5 @@
 set -e
 
-apt-get install -y graphicsmagick
 apt-get install --reinstall tzdata
 cp -f /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 rm -Rf /usr/share/zoneinfo
@@ -8,4 +7,5 @@ date
 bash $METEORD_DIR/lib/install_base.sh
 bash $METEORD_DIR/lib/install_node.sh
 bash $METEORD_DIR/lib/install_phantomjs.sh
+apt-get install -y graphicsmagick
 bash $METEORD_DIR/lib/cleanup.sh
