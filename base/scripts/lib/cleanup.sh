e930bdbcba7476d1063003da0043abb479f7547e
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-07-21 14:50:42 +0900

    Update cleanup.sh

--- base/scripts/lib/cleanup.sh
+++ base/scripts/lib/cleanup.sh
@@ -4,34 +4,14 @@ set -e
 
 printf "\n[-] Performing post install cleanup...\n\n"
 
-# Clean out docs
-rm -rf /usr/share/{doc,doc-base,man,locale,zoneinfo}
-
-# Clean out package management dirs
-rm -rf /var/lib/{cache,log}
-
-# clean additional files created outside the source tree
-rm -rf /root/{.cache,.config,.local}
-rm -rf /tmp/*
-
-# remove os dependencies
-apt-get -y autoremove
-rm -rf /var/lib/apt/lists/*
-
-printf "\n[-] Performing final cleanup...\n"
-
 # clean out docs
 rm -rf /usr/share/{doc,doc-base,man,locale,zoneinfo}
 
 # clean out package management dirs
 rm -rf /var/lib/{cache,log}
 
-# remove meteor
-rm -rf /usr/local/bin/meteor
-rm -rf /root/.meteor
-
 # clean additional files created outside the source tree
-rm -rf /root/{.npm,.cache,.config,.cordova,.local}
+rm -rf /root/{.cache,.config,.local}
 rm -rf /tmp/*
 
 # clean up npm
