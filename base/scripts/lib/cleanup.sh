dbbf1c9a45b05435fd5b67b2ba50cc8b231a5e39
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-07-21 13:09:57 +0900

    Update cleanup.sh

--- base/scripts/lib/cleanup.sh
+++ base/scripts/lib/cleanup.sh
@@ -1,17 +1,47 @@
 ## Borrowed from: https://github.com/chriswessels/meteor-tupperware
 
 set -e
-# Autoremove any junk
-apt-get autoremove -y
+
+printf "\n[-] Performing post install cleanup...\n\n"
 
 # Clean out docs
-rm -rf /usr/share/doc /usr/share/doc-base /usr/share/man /usr/share/locale /usr/share/zoneinfo
+rm -rf /usr/share/{doc,doc-base,man,locale,zoneinfo}
 
 # Clean out package management dirs
-rm -rf /var/lib/cache /var/lib/log
+rm -rf /var/lib/{cache,log}
 
-# Clean out /tmp
+# clean additional files created outside the source tree
+rm -rf /root/{.cache,.config,.local}
 rm -rf /tmp/*
 
-# Clear npm cache
-npm cache clear
+# remove os dependencies
+apt-get -y autoremove
+rm -rf /var/lib/apt/lists/*
+
+printf "\n[-] Performing final cleanup...\n"
+
+# Clean out docs
+rm -rf /usr/share/{doc,doc-base,man,locale,zoneinfo}
+
+# Clean out package management dirs
+rm -rf /var/lib/{cache,log}
+
+# remove meteor
+rm -rf /usr/local/bin/meteor
+rm -rf /root/.meteor
+
+# clean additional files created outside the source tree
+rm -rf /root/{.npm,.cache,.config,.cordova,.local}
+rm -rf /tmp/*
+
+# remove npm
+npm cache clean
+rm -rf /opt/nodejs/bin/npm
+rm -rf /opt/nodejs/lib/node_modules/npm/
+
+# remove os dependencies
+apt-get -y purge ca-certificates curl git bzip2
+apt-get -y autoremove
+apt-get -y clean
+apt-get -y autoclean
+rm -rf /var/lib/apt/lists/*
