8186a5e091ba2f1c11bae6fe3771dda39b124c23
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-07-21 13:12:24 +0900

    Update init.sh

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -5,6 +5,7 @@ cp -f /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 rm -Rf /usr/share/zoneinfo
 date
 bash $METEORD_DIR/lib/install_base.sh
+apt-get install -y libc6 graphicsmagick
 bash $METEORD_DIR/lib/install_node.sh
 bash $METEORD_DIR/lib/install_phantomjs.sh
 bash $METEORD_DIR/lib/cleanup.sh
