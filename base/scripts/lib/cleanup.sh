fcb0c59fd2455c426b8c872cf0906338e8ecfcba
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-07-21 15:33:33 +0900

    Update cleanup.sh

--- base/scripts/lib/cleanup.sh
+++ base/scripts/lib/cleanup.sh
@@ -1,25 +1,17 @@
 ## Borrowed from: https://github.com/chriswessels/meteor-tupperware
 
 set -e
+# Autoremove any junk
+apt-get autoremove -y
 
-printf "\n[-] Performing post install cleanup...\n\n"
+# Clean out docs
+rm -rf /usr/share/doc /usr/share/doc-base /usr/share/man /usr/share/locale /usr/share/zoneinfo
 
-# clean out docs
-rm -rf /usr/share/{doc,doc-base,man,locale,zoneinfo}
+# Clean out package management dirs
+rm -rf /var/lib/cache /var/lib/log
 
-# clean out package management dirs
-rm -rf /var/lib/{cache,log}
-
-# clean additional files created outside the source tree
-rm -rf /root/{.cache,.config,.local}
+# Clean out /tmp
 rm -rf /tmp/*
 
-# clean up npm
-npm cache clean
-
-# remove os dependencies
-apt-get -y purge ca-certificates curl git bzip2
-apt-get -y autoremove
-apt-get -y clean
-apt-get -y autoclean
-rm -rf /var/lib/apt/lists/*
+# Clear npm cache
+npm cache clear
