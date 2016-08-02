e54d27ca71e59777d454396fda82bb80a8fea8a6
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-08-02 16:34:49 +0900

    Update rebuild_npm_modules.sh

--- binbuild/scripts/rebuild_npm_modules.sh
+++ binbuild/scripts/rebuild_npm_modules.sh
@@ -30,7 +30,7 @@ gyp_rebuild_inside_node_modules () {
       rm -rf node_modules
       if [ -f binding.gyp ]; then
         npm install
-        node-gyp rebuild || :
+        $(npm bin -g)/node-gyp rebuild || :
       else
         npm install
       fi
