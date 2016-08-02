799efe98cd40aedc48c81a0a18cb5d6cfb320aea
Sakamoto, Kazunori <exkazuu@gmail.com>
2016-08-02 11:28:34 +0900

    Update install_binbuild_tools.sh

--- binbuild/scripts/install_binbuild_tools.sh
+++ binbuild/scripts/install_binbuild_tools.sh
@@ -2,6 +2,7 @@ set -e
 # for npm module re-building
 apt-get -y install build-essential libssl-dev git python
 npm install -g node-gyp
+ln -sf /opt/nodejs/bin/node-gyp /usr/bin/node-gyp
 # pre-install node source code for faster building
 node-gyp install ${NODE_VERSION}
 
